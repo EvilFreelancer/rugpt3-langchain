5ca624ef331c14e71f93c093750c09c7ebdaa5df
Paul Rock <paul@drteam.rocks>
2023-06-04 20:15:21 +0300

    Small tune of script

--- huggingface.py
+++ huggingface.py
@@ -1,13 +1,21 @@
 import sys
 from langchain import HuggingFacePipeline, PromptTemplate, LLMChain
 
+# Init model from folder and pass some parameters
 rugpt = HuggingFacePipeline.from_model_id(
     # The name of the model on the HuggingFace server (for example "gpt2" by default) or the path to the model on disk
     model_id="./models/dostoevsky_doesnt_write_it",
     # Other options are possible: "text2text-generation", "text-generation" (default), "summarization"
     task="text-generation",
     # Settings available for the model
-    model_kwargs={"temperature": 1.0, "max_length": 200, "top_k": 5, "top_p": 0.95, "repetition_penalty": 1.2},
+    pipeline_kwargs={
+        "temperature": 1.0,
+        "max_length": 200,
+        "repetition_penalty": 1.2,
+        "do_sample": True,
+        "top_k": 5,
+        "top_p": 0.95,
+    },
 )
 
 # Read template from file
@@ -24,7 +32,7 @@ rugpt_chain = LLMChain(prompt=prompt, llm=rugpt)
 question = sys.argv[1]
 
 # Run chain
-prediction = rugpt_chain.run(question)
+prediction = question + rugpt_chain.run(question)
 
 # Return response
 print(prediction)
